c446e49ecde496643ef1c02747b540a431fcd2d7
LittleViewer <planete.adoecrivain@gmail.com>
2025-08-20 17:15:40 +0200

    Update intensityContext.py

--- intensityContext.py
+++ intensityContext.py
@@ -40,3 +40,54 @@ class intensityContext:
     
     def returnValue(self ):
         return self.contextNumber
+    
+class thematicContext :
+
+    def __init__(self, numberContext):
+        centralThematicWord = self.findWordThematic(numberContext)
+        semantiqueCategorie = self.semantiqueCategorie(numberContext, centralThematicWord)
+        print(self.cleanerSemantiqueCategorie(semantiqueCategorie))
+
+    def findWordThematic(self, numberContext):
+        centralThematicWord = []
+        for wordPresentInContext in numberContext:
+                sectionContext = len(wordPresentInContext)
+                centralThematicWord.append(wordPresentInContext[0])
+                centralThematicWord.append(wordPresentInContext[1])
+                if sectionContext == 4:
+                    centralThematicWord.append(wordPresentInContext[2])
+        centralThematicWord = set(centralThematicWord)
+        return list(centralThematicWord)
+    
+    def semantiqueCategorie(self, numberContext, centralThematicWord):
+        familyLinkWord = []
+        for wordCentral in centralThematicWord:
+            familyLinkWord.append(wordCentral)
+            familyLinkWord.append(["After",[],"Before",[]])
+            for wordContext in numberContext:
+                for i in range(len(wordContext)):
+                    if wordCentral == wordContext[i]:
+                        for x in range(len(familyLinkWord)):
+                                if familyLinkWord[x] == wordCentral:
+                                    for z in range(len(familyLinkWord[x+1])):
+                                        if i == 0 and range(len(familyLinkWord[x+1])) == 3:
+                                            if familyLinkWord[x+1][z] == "After":
+                                                familyLinkWord[x+1][z+1].append(wordContext[i+1])
+                                                familyLinkWord[x+1][z+1] = list(set(familyLinkWord[x+1][z+1]))
+                                        if i == 1:
+                                            if familyLinkWord[x+1][z] == "Before":
+                                                familyLinkWord[x+1][z+1].append(wordContext[i-1])
+                                                familyLinkWord[x+1][z+1] = list(set(familyLinkWord[x+1][z+1]))
+                                        if i == 0 and len(familyLinkWord[x+1]) == 4 :
+                                            if familyLinkWord[x+1][z] == "After":
+                                                familyLinkWord[x+1][z+1].append(wordContext[i+1])
+                                                familyLinkWord[x+1][z+1] = list(set(familyLinkWord[x+1][z+1]))
+        return familyLinkWord
+
+    def cleanerSemantiqueCategorie(self, semantiqueCategorie):
+        for x in range(len(semantiqueCategorie)):
+                if len(semantiqueCategorie[x]) > 3:
+                    if semantiqueCategorie[x][1] == "" and semantiqueCategorie[x][3] == "":
+                        semantiqueCategorie.pop(x-1)
+                        semantiqueCategorie.pop(x)
+        return semantiqueCategorie
