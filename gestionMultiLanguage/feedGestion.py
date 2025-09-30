3810a7c0d490917426ca1e8ab205b9b8612b0189
LittleViewer <planete.adoecrivain@gmail.com>
2025-09-30 18:45:43 +0200

    finalize class for multilanguage

--- gestionMultiLanguage/feedGestion.py
+++ gestionMultiLanguage/feedGestion.py
@@ -16,8 +16,21 @@ class feedGestion :
             else: 
                 list_language_exist.add(one_obj_json["language"])
                 language_dict[one_obj_json["language"]] = [one_obj_json["rss_link"]]
-        print(language_dict)
-        print(list_language_exist)
+        return [language_dict, list_language_exist]
+    
+    def list_model_by_language(self, list_language_exist):
+        data = self.scrapFeed("gestionMultiLanguage\\languageModel.json")
+        for model in data:
+            if model["code_language"] in list_language_exist[0]:
+                list_language_exist[0][model["model_name"]] =  list_language_exist[0].pop(model["code_language"])
+        print(self.delete_not_exist_model(list_language_exist[0],list_language_exist[1]))
+    
+    def delete_not_exist_model(self, list_model_with_feed, country_code):
+        for key in list(list_model_with_feed):
+            if key in country_code:
+                list_model_with_feed.pop(key)
+        return list_model_with_feed
 
     def __init__(self):
-        self.class_by_language()
+        language_dict = self.class_by_language()
+        self.list_model_by_language(language_dict)
