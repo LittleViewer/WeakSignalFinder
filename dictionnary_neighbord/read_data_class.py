d82a66a94021d19c36844632e94d920ed79bfeb1
LittleViewer <planete.adoecrivain@gmail.com>
2026-05-24 09:23:10 +0200

    Fix filename extraction from file path
    Replace a hardcoded index when splitting Windows-style paths with taking the last path segment. Previously the code used split("\\")[7], which failed for paths with different depths; now it gets the filename with array_filename[len(array_filename)-1] to handle variable path lengths and avoid index errors.

--- dictionnary_neighbord/read_data_class.py
+++ dictionnary_neighbord/read_data_class.py
@@ -24,7 +24,8 @@ class read_data :
         all_data = []
         all_filename_read = []
         for one_path in dict_all_path:
-            all_filename_read.append(str(one_path).split("\\")[7])
+            array_filename = str(one_path).split("\\")
+            all_filename_read.append(array_filename[len(array_filename)-1])
             with open(one_path, "r") as file:
                 for line in file:
                     line = line.strip()
